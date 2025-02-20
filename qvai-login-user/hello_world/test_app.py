3095cd27603727d1e04b3f57e14c9feb1e67bc96
Yisehak Mengistu <yisehakmg@gmail.com>
2025-02-20 13:53:56 -0800

    The lambda function updated for Login and all unit test pass

--- qvai-login-user/hello_world/test_app.py
+++ qvai-login-user/hello_world/test_app.py
@@ -1,28 +1,24 @@
 import pytest
 from .app import lambda_handler  # Import your Lambda function
 
-def test_lambda_returns_200_with_valid_api_key():
-    # Simulate a valid API Gateway event with API key
+def test_lambda_with_correct_credentials():
     event = {
-        "httpMethod": "GET",
-        "headers": {
-            "x-api-key": "VALID_KEY"  # This key won't match AWS yet
-        }
+        "username": "admin",
+        "password": "password123"
     }
     
     response = lambda_handler(event, None)
     
-    # This test will FAIL because the Lambda doesn't check the API key yet
+    # This test will Pass because the Lambda return status 200 for correct credential
     assert response["statusCode"] == 200  # <--- Will fail
 
-def test_lambda_returns_403_without_api_key():
-    # Simulate a request without API key
+def test_lambda_with_incorrect_username():
     event = {
-        "httpMethod": "GET",
-        "headers": {}
+        "username": "root",
+        "password": "password123"
     }
     
     response = lambda_handler(event, None)
     
-    # This test will FAIL because the Lambda doesn't validate the API key
-    assert response["statusCode"] == 403  # <--- Will fail
+    # This test will pass because the Lambda function will give 401 for wrong credential
+    assert response["statusCode"] == 401  # <--- Will fail
